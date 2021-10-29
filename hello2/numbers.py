22b2709181257c0d625772ea292a8b1e1ae4e5b6
pcavatore <pcavatore@users.noreply.github.com>
2021-10-30 01:16:57 +0200

    difference

--- hello2/numbers.py
+++ hello2/numbers.py
@@ -2,5 +2,5 @@ print("First Number:")
 first_number = int(input())
 print("Second Number:")
 second_number = int(input())
-sum = first_number + second_number
-print("Sum: " + str(sum))
+diff = first_number - second_number
+print("Difference: " + str(diff))
